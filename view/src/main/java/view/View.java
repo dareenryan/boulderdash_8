981d63bbac5ec4922e3e6bd1b968de295e2344c4
Etienne <etienne.mayack@2027.ucac-icam.com>
2023-11-14 14:49:36 +0100

    1ere modifications du package vue pour essai avec le controller et le contract

--- view/src/main/java/view/View.java
+++ view/src/main/java/view/View.java
@@ -1,82 +1,309 @@
 package view;
 
-import java.awt.event.KeyEvent;
-
-import javax.swing.SwingUtilities;
-
 import contract.ControllerOrder;
-import contract.IController;
-import contract.IModel;
+import contract.IOrderPerformer;
 import contract.IView;
+import entity.IMap;
+import fr.exia.showboard.BoardFrame;
+import model.IMobile;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+
 
 /**
- * The Class View.
+ * <h1>The View Class.</h1>
  *
- * @author Jean-Aymeric Diet
+ * @author Groupe 1
+ * @version 0.1
  */
-public final class View implements IView, Runnable {
 
-	/** The frame. */
-	private final ViewFrame viewFrame;
+public class View implements Runnable, KeyListener, IView {
 
 	/**
-	 * Instantiates a new view.
-	 *
-	 * @param model
-	 *          the model
+	 * The Constant mapView.
 	 */
-	public View(final IModel model) {
-		this.viewFrame = new ViewFrame(model);
+	private static final int mapView = 31;
+
+	/**
+	 * The Constant closeView.
+	 */
+	private Rectangle closeView;
+
+	/**
+	 * The map.
+	 */
+	private IMap map;
+
+	/**
+	 * My player.
+	 */
+	private IMobile myPlayer;
+
+	/**
+	 * The view.
+	 */
+	private int view;
+
+	/**
+	 * The order performer.
+	 */
+	private IOrderPerformer orderPerformer;
+
+	final BoardFrame boardFrame = new BoardFrame("BoulderDash");
+
+	/**
+	 * Instantiates a new boulderdash View.
+	 *
+	 * @param map      the map
+	 * @param myPlayer the my player
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	public View(final IMap map, final IMobile myPlayer) throws IOException {
+		this.setView(mapView);
+		this.setMap(map);
+		this.setMyPlayer(myPlayer);
+		this.getMyPlayer().getSprite().loadImage();
+		this.setCloseView(new Rectangle(0, this.getMyPlayer().getY(), this.getMap().getWidth(), mapView));
 		SwingUtilities.invokeLater(this);
 	}
 
-	/**
-	 * Key code to controller order.
-	 *
-	 * @param keyCode
-	 *          the key code
-	 * @return the controller order
+	public static UserOrder keyCodeToControllerOrder(int keyCode) {
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see fr.exia.boulderdash.contract.intview.IView#displayMessage(java.lang.String)
 	 */
-	protected static ControllerOrder keyCodeToControllerOrder(final int keyCode) {
-		switch (keyCode) {
-			case KeyEvent.VK_G:
-				return ControllerOrder.English;
-			case KeyEvent.VK_F:
-				return ControllerOrder.Francais;
-			case KeyEvent.VK_D:
-				return ControllerOrder.Deutsch;
-			case KeyEvent.VK_I:
-				return ControllerOrder.Indonesia;
-			default:
-				return ControllerOrder.English;
+	@Override
+	public final void displayMessage(final String message) {
+		JOptionPane.showMessageDialog(null, message);
+		System.exit(0);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public final void run() {
+		boardFrame.setDimension(new Dimension(this.getMap().getWidth(), this.getMap().getHeight()));
+		boardFrame.setDisplayFrame(this.closeView);
+		boardFrame.setLocation(500, 50);
+		boardFrame.setSize(1000, 1000);
+		boardFrame.setHeightLooped(false);
+		boardFrame.addKeyListener(this);
+		boardFrame.setFocusable(true);
+		boardFrame.setFocusTraversalKeysEnabled(false);
+
+		refresh();
+
+		boardFrame.addPawn(this.getMyPlayer());
+		this.getMap().getObservable().addObserver(boardFrame.getObserver());
+		this.followMyPlayer();
+		boardFrame.setVisible(true);
+	}
+
+	public final void refresh() {
+		for (int x = 0; x < this.getMap().getWidth(); x++) {
+			for (int y = 0; y < this.getMap().getHeight(); y++) {
+				boardFrame.addSquare(this.map.getOnTheMapXY(x, y), x, y);
+			}
 		}
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Prints the map and the player's character in the console.
 	 *
-	 * @see contract.IView#printMessage(java.lang.String)
+	 * @param yStart the y start
 	 */
-	public void printMessage(final String message) {
-		this.viewFrame.printMessage(message);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.lang.Runnable#run()
-	 */
-	public void run() {
-		this.viewFrame.setVisible(true);
+	public final void show(final int yStart) {
+		int y = yStart % this.getMap().getHeight();
+		for (int view = 0; view < this.getView(); view++) {
+			for (int x = 0; x < this.getMap().getWidth(); x++) {
+				if ((x == this.getMyPlayer().getX()) && (y == yStart)) {
+					System.out.print(this.getMyPlayer().getSprite().getConsoleImage());
+				} else {
+					System.out.print(this.getMap().getOnTheMapXY(x, y).getSprite().getConsoleImage());
+				}
+			}
+			y = (y + 1) % this.getMap().getHeight();
+			System.out.print("\n");
+		}
 	}
 
 	/**
-	 * Sets the controller.
+	 * Key code to user order.
 	 *
-	 * @param controller
-	 *          the new controller
+	 * @param keyCode the key code
+	 * @return the user order
 	 */
-	public void setController(final IController controller) {
-		this.viewFrame.setController(controller);
+	private ControllerOrder keyCodeToUserOrder(final int keyCode) {
+		ControllerOrder ControllerOrder = null;
+		switch (keyCode) {
+			case KeyEvent.VK_RIGHT:
+				ControllerOrder = ControllerOrder.RIGHT;
+				refresh();
+				break;
+			case KeyEvent.VK_LEFT:
+				ControllerOrder = ControllerOrder.LEFT;
+				refresh();
+				break;
+			case KeyEvent.VK_DOWN:
+				ControllerOrder = ControllerOrder.DOWN;
+				refresh();
+				break;
+			case KeyEvent.VK_UP:
+				ControllerOrder = ControllerOrder.UP;
+				refresh();
+				break;
+			default:
+				ControllerOrder = ControllerOrder.NOP;
+				refresh();
+				break;
+		}
+		return ControllerOrder;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.awt.event.KeyListener#keyTyped(java.awt.event.KeyEvent)
+	 */
+	@Override
+	public void keyTyped(final KeyEvent keyEvent) {
+		// Nop
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
+	 */
+	@Override
+	public final void keyPressed(final KeyEvent keyEvent) {
+		this.getOrderPerformer().orderPerform(keyCodeToControllerOrder(keyEvent.getKeyCode()));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.awt.event.KeyListener#keyReleased(java.awt.event.KeyEvent)
+	 */
+	@Override
+	public void keyReleased(final KeyEvent keyEvent) {
+		// Nop
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see fr.exia.boulderdash.contract.intview.IView#followMyvehicle()
+	 */
+	@Override
+	public final void followMyPlayer() {
+		this.getCloseView().y = this.getMyPlayer().getY() - this.getMap().getHeight() / 2;
+		this.getCloseView().x = this.getMyPlayer().getX() - this.getMap().getHeight() / 2;
+	}
+
+	/**
+	 * Gets the map.
+	 *
+	 * @return the map
+	 */
+	private IMap getMap() {
+		return this.map;
+	}
+
+	/**
+	 * Sets the map.
+	 *
+	 * @param map the new map
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	private void setMap(final IMap map) throws IOException {
+		this.map = map;
+		for (int x = 0; x < this.getMap().getWidth(); x++) {
+			for (int y = 0; y < this.getMap().getHeight(); y++) {
+				this.getMap().getOnTheMapXY(x, y).getSprite().loadImage();
+			}
+		}
+	}
+
+	/**
+	 * Gets myPlayer.
+	 *
+	 * @return myPlayer
+	 */
+	private IMobile getMyPlayer() {
+		return this.myPlayer;
+	}
+
+	/**
+	 * Sets myPlayer.
+	 *
+	 * @param myPlayer my new player
+	 */
+	private void setMyPlayer(final IMobile myPlayer) {
+		this.myPlayer = myPlayer;
+	}
+
+	/**
+	 * Gets the view.
+	 *
+	 * @return the view
+	 */
+	private int getView() {
+		return this.view;
+	}
+
+	/**
+	 * Sets the view.
+	 *
+	 * @param view the new view
+	 */
+	private void setView(final int view) {
+		this.view = view;
+	}
+
+	/**
+	 * Gets the close view.
+	 *
+	 * @return the close view
+	 */
+	private Rectangle getCloseView() {
+		return this.closeView;
+	}
+
+	/**
+	 * Sets the close view.
+	 *
+	 * @param closeView the new close view
+	 */
+	private void setCloseView(final Rectangle closeView) {
+		this.closeView = closeView;
+	}
+
+	/**
+	 * Gets the order performer.
+	 *
+	 * @return the order performer
+	 */
+	private IOrderPerformer getOrderPerformer() {
+		return this.orderPerformer;
+	}
+
+	/**
+	 * Sets the order performer.
+	 *
+	 * @param orderPerformer the new order performer
+	 */
+	public final void setOrderPerformer(final IOrderPerformer orderPerformer) {
+		this.orderPerformer = orderPerformer;
+	}
+
+	@Override
+	public void printMessage(String s) {
+
 	}
 }
--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -1,174 +1,76 @@
 package view;
 
-import java.awt.GraphicsConfiguration;
-import java.awt.HeadlessException;
+import fr.exia.showboard.IPawn;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+public class ViewFrame extends JFrame implements KeyListener {
 
-import contract.IController;
-import contract.IModel;
+	private static final long serialVersionUID = -697358409737458175L;
 
-/**
- * The Class ViewFrame.
- *
- * @author Jean-Aymeric Diet
- */
-class ViewFrame extends JFrame implements KeyListener {
+	private Dimension dimension;
+	private Rectangle displayFrame;
 
-	/** The model. */
-	private IModel						model;
-
-	/** The controller. */
-	private IController				controller;
-	/** The Constant serialVersionUID. */
-	private static final long	serialVersionUID	= -697358409737458175L;
-
-	/**
-	 * Instantiates a new view frame.
-	 *
-	 * @param model
-	 *          the model
-	 * @throws HeadlessException
-	 *           the headless exception
-	 */
-	public ViewFrame(final IModel model) throws HeadlessException {
-		this.buildViewFrame(model);
-	}
-
-	/**
-	 * Instantiates a new view frame.
-	 *
-	 * @param model
-	 *          the model
-	 * @param gc
-	 *          the gc
-	 */
-	public ViewFrame(final IModel model, final GraphicsConfiguration gc) {
-		super(gc);
-		this.buildViewFrame(model);
-	}
-
-	/**
-	 * Instantiates a new view frame.
-	 *
-	 * @param model
-	 *          the model
-	 * @param title
-	 *          the title
-	 * @throws HeadlessException
-	 *           the headless exception
-	 */
-	public ViewFrame(final IModel model, final String title) throws HeadlessException {
+	public ViewFrame(final String title) throws HeadlessException {
 		super(title);
-		this.buildViewFrame(model);
+		this.buildViewFrame();
+		this.addKeyListener(this);
+		this.requestFocus();
 	}
 
-	/**
-	 * Instantiates a new view frame.
-	 *
-	 * @param model
-	 *          the model
-	 * @param title
-	 *          the title
-	 * @param gc
-	 *          the gc
-	 */
-	public ViewFrame(final IModel model, final String title, final GraphicsConfiguration gc) {
-		super(title, gc);
-		this.buildViewFrame(model);
-	}
-
-	/**
-	 * Gets the controller.
-	 *
-	 * @return the controller
-	 */
-	private IController getController() {
-		return this.controller;
-	}
-
-	/**
-	 * Sets the controller.
-	 *
-	 * @param controller
-	 *          the new controller
-	 */
-	protected void setController(final IController controller) {
-		this.controller = controller;
-	}
-
-	/**
-	 * Gets the model.
-	 *
-	 * @return the model
-	 */
-	protected IModel getModel() {
-		return this.model;
-	}
-
-	/**
-	 * Sets the model.
-	 *
-	 * @param model
-	 *          the new model
-	 */
-	private void setModel(final IModel model) {
-		this.model = model;
-	}
-
-	/**
-	 * Builds the view frame.
-	 *
-	 * @param model
-	 *          the model
-	 */
-	private void buildViewFrame(final IModel model) {
-		this.setModel(model);
+	private void buildViewFrame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
-		this.addKeyListener(this);
-		this.setContentPane(new ViewPanel(this));
-		this.setSize(400 + this.getInsets().left + this.getInsets().right, 60 + this.getInsets().top + this.getInsets().bottom);
-		this.setLocationRelativeTo(null);
+		this.setDimension(new Dimension(1, 1));
 	}
 
-	/**
-	 * Prints the message.
-	 *
-	 * @param message
-	 *          the message
-	 */
-	public void printMessage(final String message) {
+	@Override
+	public final void keyTyped(final KeyEvent keyEvent) {
+		// Nop
+	}
+
+	@Override
+	public final void keyPressed(final KeyEvent keyEvent) {
+		// Nop
+	}
+
+	@Override
+	public final void keyReleased(final KeyEvent keyEvent) {
+		// Nop
+	}
+
+	public final void displayMessage(final String message) {
 		JOptionPane.showMessageDialog(null, message);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.awt.event.KeyListener#keyTyped(java.awt.event.KeyEvent)
-	 */
-	public void keyTyped(final KeyEvent e) {
-
+	public final Dimension getDimension() {
+		return this.dimension;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
-	 */
-	public void keyPressed(final KeyEvent e) {
-		this.getController().orderPerform(View.keyCodeToControllerOrder(e.getKeyCode()));
+	public final void setDimension(final Dimension dimension) {
+		this.dimension = dimension;
+		this.setSize(dimension);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.awt.event.KeyListener#keyReleased(java.awt.event.KeyEvent)
-	 */
-	public void keyReleased(final KeyEvent e) {
+	public final Rectangle getDisplayFrame() {
+		return this.displayFrame;
+	}
 
+	public final void setDisplayFrame(final Rectangle displayFrame) {
+		this.displayFrame = displayFrame;
+	}
+
+	public final void addPawn(final IPawn pawn) {
+		final ViewPanel viewPanel = new ViewPanel(pawn);
+		this.setContentPane(viewPanel);
+		this.setSize(pawn.getWidth(), pawn.getHeight());
+		this.setLocationRelativeTo(null);
+	}
+
+	public final void removeAllPawns() {
+		this.setContentPane(new JPanel());
 	}
 }
